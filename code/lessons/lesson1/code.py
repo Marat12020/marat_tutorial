68e0578b29b324d304f456dd2e9c04fbba57c756
Marat12020 <maratik080212@gmail.com>
2023-09-24 11:51:44 +0300

    написал программу для решения квадратных уравнений

--- code/lessons/lesson1/code.py
+++ code/lessons/lesson1/code.py
@@ -1,2 +1,16 @@
-for i in range(50):
-    print("Hello")
+a=1
+b=-2
+c=-3
+D=b**2-(4*a*c)
+print(D)
+count=0
+if D==0:
+    count=1
+if D>0:
+    count=2
+print(count)
+k=1
+for i in range(count):
+    x=(-b+k*D**0.5)/a
+    k=-k
+    print(x)
